078f2bd9591a0e362250fe034c715e1d93cfe693
Chananchida.Sangaram <chananchidas@cp0293.irc.ugent.be>
2021-02-15 23:05:56 +0100

    minor changes

--- Scripts/server/copy_files.sh
+++ Scripts/server/copy_files.sh
@@ -1,10 +1,10 @@
-for dir in LocationModelLinearDependent*/
+for dir in data/brain_cortex_generation/cell2location_results/std_model/LocationModelLinearDependent*/
 do
     term='genes_(\w+)/'
     [[ $dir =~ $term ]]
-    newname="allen_cortex_dwn_${BASH_REMATCH[1]}_cell2location.csv"
-    # echo $dir
+    newname="brain_cortex_generation_${BASH_REMATCH[1]}_cell2location.csv"
+    echo $dir
     # echo "${BASH_REMATCH[1]}"
     echo $newname
-    cp $dir/W_cell_density_q05.csv allen_cortex_dwn_results/$newname
+    cp $dir/W_cell_density_q05.csv data/brain_cortex_generation/cell2location_results_compiled/$newname
 done
