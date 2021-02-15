c4017272f2345e0447e7e1ee8e0e5e310bb32863
Chananchida.Sangaram <chananchidas@cp0293.irc.ugent.be>
2021-02-15 23:03:28 +0100

    added argparser to run cell2location :sparkles:

--- Scripts/server/run_cell2location_regression.py
+++ Scripts/server/run_cell2location_regression.py
@@ -1,91 +1,114 @@
-##### COMMAND LINE PART #####
+import argparse as arp
 import sys
-
-if len(sys.argv) != 2:
-    sys.stderr.write("USAGE: python %s <CUDA_DEVICE_ID> \n" % sys.argv[0])
-    sys.exit(1)
-
-cuda_device = sys.argv[1]
-
-assert cuda_device in ["0", "1", "2"], "invalid device id"
-
-##### MAIN PART #####
-
 import os
-os.environ["CUDA_VISIBLE_DEVICES"]=cuda_device
 
-import sys
-import scanpy as sc
-import anndata
-import pandas as pd
-import numpy as np
+def main():
+    ##### PARSING COMMAND LINE ARGUMENTS #####
+    prs = arp.ArgumentParser()
+    
+    prs.add_argument('sc_data_path',
+                     type = str, help = 'path to single cell h5ad count data')
+    
+    prs.add_argument('cuda_device', type = str, help = "index of cuda device ID, from 0-7")
 
-data_type = 'float32'
+    prs.add_argument('-o','--out_dir', default = None,
+                     type = str, help = 'directory for regression model')
 
-import cell2location
-import matplotlib as mpl
-from matplotlib import rcParams
-import matplotlib.pyplot as plt
-import seaborn as sns
+    prs.add_argument('-a','--annotation_column', default = 'celltype',
+                 type = str, help = 'column name for covariate')
+    
+    args = prs.parse_args()
+    cuda_device = args.cuda_device
 
-# silence scanpy that prints a lot of warnings
-import warnings
-warnings.filterwarnings('ignore')
+    assert cuda_device in ["0", "1", "2", "3", "4", "5", "6", "7"], "invalid device id"
+    
+    ## scRNA reference (raw counts)
+    sc_data_path = args.sc_data_path
+    import anndata
+    adata_scrna_raw = anndata.read_h5ad(sc_data_path)
+    
+    if args.out_dir is None:
+        results_folder = os.path.dirname(sc_data_path) + '/cell2location_results/'
+    else:
+        results_folder = args.out_dir
 
-sc_data_folder = '/srv/scratch/chananchidas/data/raw/'
-results_folder = '/srv/scratch/chananchidas/cell2location_results/'
+    if not os.path.exists(results_folder):
+        os.makedirs(results_folder)
+        
+    celltype = args.annotation_column
+    
+    ##### MAIN CODE #####
+    os.environ["CUDA_VISIBLE_DEVICES"]=cuda_device
 
-## snRNA reference (raw counts)
-adata_scrna_raw = anndata.read_h5ad(sc_data_folder + "allen_cortex_dwn_original.h5ad")
+    import sys
+    import scanpy as sc
+    import pandas as pd
+    import numpy as np
 
-# Reduce the number of genes by discarding lowly expressed genes
-# remove cells and genes with 0 counts everywhere
-sc.pp.filter_cells(adata_scrna_raw, min_genes=1)
-sc.pp.filter_genes(adata_scrna_raw, min_cells=1)
+    data_type = 'float32'
 
-# calculate the mean of each gene across non-zero cells
-adata_scrna_raw.var['n_cells'] = (adata_scrna_raw.X.toarray() > 0).sum(0)
-adata_scrna_raw.var['nonz_mean'] = adata_scrna_raw.X.toarray().sum(0) / adata_scrna_raw.var['n_cells']
-nonz_mean_cutoff = 0.05
-cell_count_cutoff = np.log10(adata_scrna_raw.shape[0] * 0.0005)
-cell_count_cutoff2 = np.log10(adata_scrna_raw.shape[0] * 0.03)
+    import cell2location
+    import matplotlib as mpl
+    from matplotlib import rcParams
+    import matplotlib.pyplot as plt
+    import seaborn as sns
 
-# select genes based on mean expression in non-zero cells
-adata_scrna_raw = adata_scrna_raw[:,(np.array(np.log10(adata_scrna_raw.var['nonz_mean']) > nonz_mean_cutoff)
-         | np.array(np.log10(adata_scrna_raw.var['n_cells']) > cell_count_cutoff2))
-      & np.array(np.log10(adata_scrna_raw.var['n_cells']) > cell_count_cutoff)]
-# add count matrix as raw
-adata_scrna_raw.raw = adata_scrna_raw
+    # silence scanpy that prints a lot of warnings
+    import warnings
+    warnings.filterwarnings('ignore')
 
-# Run the pipeline
-from cell2location import run_regression
-r, adata_scrna_raw = run_regression(adata_scrna_raw, # input data object]
-                   
-                   verbose=True, return_all=True,
-                                 
-                   train_args={
-                    'covariate_col_names': ['subclass'], # column listing cell type annotation
-                    'sample_name_col': 'donor', # column listing sample ID for each cell
-                    
-                    # column listing technology, e.g. 3' vs 5', 
-                    # when integrating multiple single cell technologies corresponding 
-                    # model is automatically selected
-                    'tech_name_col': None, 
-                    
-                    'stratify_cv': 'subclass', # stratify cross-validation by cell type annotation
-                       
-                    'n_epochs': 100, 'minibatch_size': 1024, 'learning_rate': 0.01,
-                       
-                    'use_cuda': True, # use GPU?
-                       
-                    'train_proportion': 0.9, # proportion of cells in the training set (for cross-validation)
-                    'l2_weight': True,  # uses defaults for the model
-                    
-                    'use_raw': True},
-                                 
-                   model_kwargs={}, # keep defaults
-                   posterior_args={}, # keep defaults
-                                 
-                   export_args={'path': results_folder + 'regression_model/', # where to save results
-                                'save_model': True, # save pytorch model?
-                                'run_name_suffix': ''})
+    # Reduce the number of genes by discarding lowly expressed genes
+    # remove cells and genes with 0 counts everywhere
+    sc.pp.filter_cells(adata_scrna_raw, min_genes=1)
+    sc.pp.filter_genes(adata_scrna_raw, min_cells=1)
+
+    # calculate the mean of each gene across non-zero cells
+    adata_scrna_raw.var['n_cells'] = (adata_scrna_raw.X.toarray() > 0).sum(0)
+    adata_scrna_raw.var['nonz_mean'] = adata_scrna_raw.X.toarray().sum(0) / adata_scrna_raw.var['n_cells']
+    nonz_mean_cutoff = 0.05
+    cell_count_cutoff = np.log10(adata_scrna_raw.shape[0] * 0.0005)
+    cell_count_cutoff2 = np.log10(adata_scrna_raw.shape[0] * 0.03)
+
+    # select genes based on mean expression in non-zero cells
+    adata_scrna_raw = adata_scrna_raw[:,(np.array(np.log10(adata_scrna_raw.var['nonz_mean']) > nonz_mean_cutoff)
+             | np.array(np.log10(adata_scrna_raw.var['n_cells']) > cell_count_cutoff2))
+          & np.array(np.log10(adata_scrna_raw.var['n_cells']) > cell_count_cutoff)]
+    # add count matrix as raw
+    adata_scrna_raw.raw = adata_scrna_raw
+
+    # Run the pipeline
+    from cell2location import run_regression
+    r, adata_scrna_raw = run_regression(adata_scrna_raw, # input data object]
+
+                       verbose=True, return_all=True,
+
+                       train_args={
+                        'covariate_col_names': [celltype], # column listing cell type annotation
+                        #'sample_name_col': 'donor', # column listing sample ID for each cell
+
+                        # column listing technology, e.g. 3' vs 5', 
+                        # when integrating multiple single cell technologies corresponding 
+                        # model is automatically selected
+                        'tech_name_col': None, 
+
+                        'stratify_cv': celltype, # stratify cross-validation by cell type annotation
+
+                        'n_epochs': 100, 'minibatch_size': 1024, 'learning_rate': 0.01,
+
+                        'use_cuda': True, # use GPU?
+
+                        'train_proportion': 0.9, # proportion of cells in the training set (for cross-validation)
+                        'l2_weight': True,  # uses defaults for the model
+
+                        'use_raw': True},
+
+                       model_kwargs={}, # keep defaults
+                       posterior_args={}, # keep defaults
+
+                       export_args={'path': results_folder + 'regression_model/', # where to save results
+                                    'save_model': True, # save pytorch model?
+                                    'run_name_suffix': ''})
+
+
+if __name__ == '__main__':
+    main()
