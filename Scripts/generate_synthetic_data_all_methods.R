1ec9146b1ebe44d92124dff829d5001155dcc025
csangara <sai.chananchida@gmail.com>
2021-05-13 23:19:28 +0700

    organized files that check for distributions of synthetic data
    split the generate_synthetic_data into two, replaced check_distributions_all_methods with check_distributions_fitdistR (because the former is a replicate of server/check_distributions_countsimQC.R)

--- Scripts/generate_synthetic_data_all_methods.R
+++ Scripts/generate_synthetic_data_all_methods.R
@@ -1,134 +1,35 @@
 library(Seurat)
-library(fitdistrplus)
 setwd("D:/Work (Yr 2 Sem 1)/Thesis/")
 
-getDistributionInfo <- function(data, path, dataset_name, dist_type="discrete"){
-  png(paste0(path, dataset_name, "_hist_cumdist.png"), width=800, height=600)
-  plotdist(data, histo = TRUE, demp = TRUE)
-  mtext(dataset_name, line=3)
-  dev.off()
-  # descdist(data, boot=1000) # another plot
-  
-  distributions <- c("norm", "pois", "nbinom")
-  plot_legend <- c("normal", "poisson", "negative binomial")
-  if (dist_type == "continuous") { 
-    distributions <- c("norm", "beta", "gamma")
-    data <- normalize(data)
-    plot_legend <- c("normal", "beta", "gamma")
-  }
-  
-  all_models <- list()
-  for (distribution in distributions) {
-    all_models[[distribution]] = fitdist(data, distribution)
-  }
-  
-  png(paste0(path, dataset_name, "_QQplot_", dist_type, ".png"))
-  # also denscomp(), cdfcomp(), ppcomp() but qq seems the most useful
-  qqcomp(all_models, legendtext = plot_legend,
-         main=paste("QQ-plot for", dataset_name))
-  dev.off()
-  
-  gof <- gofstat(all_models, fitnames=distributions)
-  gof_df <- data.frame(gof[c("chisq", "chisqpvalue", "ks", "cvm", "ad", "bic", "aic")])
-  write.table(matrix(paste("GOF statistics for", dataset_name)),
-              file=paste0(path, dataset_name, "_", dist_type, "_GOF.txt"),
-              col.names=FALSE, row.names=FALSE, quote=FALSE)
-  
-  write.table(round(gof_df, 4), file=paste0(path, dataset_name,  "_", dist_type, "_GOF.txt"),
-              quote=FALSE, sep="\t", append=TRUE)
-  write.table(matrix(c("ks = Kolomogrov-Smirnov statistic, cvm=Cramer-von Mises statistic, ad=Anderson-Darling statistic,
-                       bic=Bayesian Information Criterion, aic=Akaike's Information criterion",
-                       "ks, cvm, and ad are recommended for continuous data, while chisq is for discrete ones")),
-              file=paste0(path, dataset_name,  "_", dist_type, "_GOF.txt"),
-              col.names=FALSE, row.names=FALSE, quote=FALSE, append=TRUE)  
-  
-}
-
-# Convert data from 0 to 1
-normalize <- function(x) {
-  x_scaled <- (x - min(x) + 0.001) / (max(x) - min(x) + 0.002)
-  return(x_scaled)
-}
-
-###### CHECK DISTRIBUTION OF VISIUM DATA ###### 
-# Histograms for brain sections "stxBrain"
-library(SeuratData)
-sections = c("posterior1", "posterior2", "anterior1", "anterior2")
-stats = data.frame("mean", "sd")
-
-for (i in 1:4){
-  section = sections[i]
-  brain <- LoadData("stxBrain", type = section)
-  brain <- colSums(as.matrix(GetAssayData(brain)))
-  png(file=paste0("plots/data_distribution/stxBrain_", section, "_hist.png"))
-  print(hist(brain, main=paste0("UMI distribution per spot of stxBrain, ", section)))
-  mtext(paste0("Mean: ", round(mean(brain),2),
-               "; SD: ", round(sd(brain), 2)), side=3)
-  dev.off()
-  getDistributionInfo(brain, "plots/data_distribution/", paste0("stxBrain_", section), "continuous")
-  getDistributionInfo(brain, "plots/data_distribution/", paste0("stxBrain_", section), "discrete")
-  stats[i,] = c(mean(brain), sd(brain))
-}
-
-# Histogram for "stxKidney"
-kidney <- LoadData("stxKidney")
-kidney <- colSums(as.matrix(GetAssayData(kidney)))
-png(file="plots/data_distribution/stxKidney_hist.png")
-print(hist(kidney, main="UMI distribution per spot of stxKidney"))
-mtext(paste0("Mean: ", round(mean(kidney),2),
-             "; SD: ", round(sd(kidney), 2)), side=3)
-dev.off()
-print(c(mean(kidney), sd(kidney)))
-getDistributionInfo(kidney, "plots/data_distribution/", "stxKidney", "continuous")
-getDistributionInfo(kidney, "plots/data_distribution/", "stxKidney", "discrete")
-
-###### CHECK DISTRIBUTION OF SCRNA-SEQ DATA ###### 
-# SMART-Seq mouse brain data
-allen_cortex <- readRDS("rds/allen_cortex.rds")
-allen_cortex <- colSums(as.matrix(GetAssayData(allen_cortex)))
-print(hist(allen_cortex, main="UMI distribution per cell of SMART-Seq scRNA brain"))
-print(c(mean(allen_cortex), sd(allen_cortex)))
-
-# 10x scRNA-seq mouse brain data (Robin's file)
-brain_sc_10x <- readRDS("rds/seurat_obj_scrnaseq_cortex_filtered.rds")
-DefaultAssay(brain_sc_10x) <- "RNA"
-brain_sc_10x <- colSums(as.matrix(GetAssayData(brain_sc_10x)))
-print(hist(brain_sc_10x, main="UMI distribution per cell of 10x scRNA brain"))
-print(c(mean(brain_sc_10x), sd(brain_sc_10x)))
-
-# Create new object with rawcounts (to reduce file size)
-brain_sc_10x <- readRDS("rds/seurat_obj_scrnaseq_cortex_filtered.rds")
-new_brain <- CreateSeuratObject(counts=brain_sc_10x@assays$RNA@counts, meta.data = brain_sc_10x@meta.data)
-saveRDS(new_brain, "rds/seurat_obj_scrnaseq_cortex_filtered_rawcounts.rds")
-
-###### GENERATING SYNTHETIC SPOTS ######
-## VISIUM ##
+###### GENERATING SYNTHETIC SPOTS OF DIFFERENT METHODS ######
+## SYNTHVISIUM ##
 library(synthvisium)
 brain_sc_10x <- readRDS("rds/seurat_obj_scrnaseq_cortex_filtered_rawcounts.rds")
 
 set.seed(10)
 synthetic_visium_data = generate_synthetic_visium(seurat_obj = brain_sc_10x, dataset_type = "artificial_diverse_distinct",
-                                                  clust_var = "subclass", n_regions = 5, n_spots_min = 50, n_spots_max = 500,
-                                                  visium_mean = 30000, visium_sd = 8000)
-png(file="plots/data_distribution/synthvisium_mean30000,sd8000_hist.png")
-print(hist(colSums(synthetic_visium_data$counts), main="UMI distribution per spot of synthvisium synthetic data"))
-mtext(paste0("Mean: ", round(mean(colSums(synthetic_visium_data$counts)),2),
-             "; SD: ", round(sd(colSums(synthetic_visium_data$counts)), 2)), side=3)
-dev.off()
-getDistributionInfo(colSums(synthetic_visium_data$counts), "plots/data_distribution/",
-                    "synthvisium_mean30000,sd8000", "continuous")
-getDistributionInfo(colSums(synthetic_visium_data$counts), "plots/data_distribution/",
-                    "synthvisium_mean30000,sd8000", "discrete")
+                                                  clust_var = "subclass", n_regions = 5, n_spots_min = 600, n_spots_max = 700,
+                                                  visium_mean = 20000, visium_sd = 6000)
+saveRDS(synthetic_visium_data, "Data/synthetic_datasets/for_comparing_dists/synthvisium_sc_10x_braincortex_robin_rawcounts_3224spots.rds")
+
+#kidney
+kidney <- readRDS("Scripts/robin/raw_data/scRNAseq/seurat_obj_scrnaseq_kidney_filtered.rds")
+kidney <- CreateSeuratObject(GetAssayData(kidney, slot="counts"), meta.data = data.frame(kidney$celltype))
+
+synthetic_visium_data = generate_synthetic_visium(seurat_obj = kidney, dataset_type = "artificial_diverse_distinct",
+                                                  clust_var = "kidney.celltype", n_regions = 3, n_spots_min = 400, n_spots_max = 500,
+                                                  visium_mean = 35000, visium_sd = 8000)
+saveRDS(synthetic_visium_data, "Data/synthetic_datasets/for_comparing_dists/synthvisium_sc_10x_kidney_robin_rawcounts_1314spots.rds")
 
 ## SPOTLIGHT ##
 source("Scripts/synthetic_data_generation/spotlight_test_spot_fun.R")
 brain_sc_10x <- readRDS("rds/seurat_obj_scrnaseq_cortex_filtered_rawcounts.rds")
-spotlight_synth <- test_spot_fun(brain_sc_10x, clust_vr="subclass", n=2500)
-# saveRDS(spotlight_synth, "Data/synthetic_datasets/spotlight_sc_10x_braincortex_robin_rawcounts_2500spots.rds")
-hist(colSums(spotlight_synth$topic_profiles), breaks = c(16000, 18000, 20000, 22000),
-     main="UMI distribution per spot of SPOTlight synthetic data")
+spotlight_synth <- test_spot_fun(brain_sc_10x, clust_vr="subclass", n=3500)
+saveRDS(spotlight_synth, "Data/synthetic_datasets/for_comparing_dists/spotlight_sc_10x_braincortex_robin_rawcounts_3500spots.rds")
 
-#spotlight_synth <- readRDS("Data/synthetic_datasets/spotlight_sc_10x_braincortex_robin_rawcounts_2500spots.rds")
+kidney <- readRDS("Scripts/robin/raw_data/scRNAseq/seurat_obj_scrnaseq_kidney_filtered.rds")
+spotlight_synth <- test_spot_fun(kidney, clust_vr="celltype", n=1500)
+saveRDS(spotlight_synth, "Data/synthetic_datasets/for_comparing_dists/spotlight_sc_10x_kidney_robin_rawcounts_1500spots.rds")
 
 ## STEREOSCOPE ##
 # need two .tsv files, also at least 30 cells required per cell type
@@ -147,21 +48,24 @@ metadata <- data.frame("cell" = colnames(brain_sc_10x),
 write.table(metadata, file="rds/seurat_obj_scrnaseq_cortex_filtered_rawcounts_metadata.tsv",
             quote=FALSE, sep="\t", col.names=TRUE, row.names=FALSE)
 
-# Run the make_st_set.py file using these two files #
-# Read in results (there are three files)
-expression_data <- read.table("Data/synthetic_datasets/stereoscope_sc_10x_braincortex_robin_rawcounts_default/counts.st_synth.tsv",
+# kidney
+kidney <- readRDS("Scripts/robin/raw_data/scRNAseq/seurat_obj_scrnaseq_kidney_filtered.rds")
+selected_celltypes <- names(table(kidney$celltype)[table(kidney$celltype) >= 30])
+Idents(kidney) <- kidney$celltype
+kidney <- subset(kidney, idents = selected_celltypes) # Select cell types >= 30 cells
+write.table(t(as.matrix(kidney[["RNA"]]@counts)),
+            file="rds/seurat_obj_scrnaseq_kidney_filtered_rawcounts_counts.tsv",
+            quote=FALSE, sep="\t", col.names=TRUE, row.names=TRUE)
+metadata <- data.frame("cell" = colnames(kidney),
+                       "bio_celltype" = kidney$celltype)
+write.table(metadata, file="rds/seurat_obj_scrnaseq_kidney_filtered_rawcounts_metadata.tsv",
+            quote=FALSE, sep="\t", col.names=TRUE, row.names=FALSE)
+
+# Run the make_st_set.py file using these two files!
+# Read expression data
+expression_data <- read.table("Data/synthetic_datasets/for_comparing_dists/stereoscope_sc_10x_braincortex_robin_rawcounts_default/counts.st_synth.tsv",
                               header=TRUE, sep="\t", row.names=1)
-png(file="plots/data_distribution/stereoscope_default_hist.png")
-print(hist(rowSums(expression_data), main="UMI distribution per spot of stereoscope synthetic data"))
-mtext(paste0("Mean: ", round(mean(rowSums(expression_data)),2),
-             "; SD: ", round(sd(rowSums(expression_data)), 2)), side=3)
-dev.off()
-print(paste0("Mean: ", round(mean(rowSums(expression_data)),2),
-             "; SD: ", round(sd(rowSums(expression_data)), 2)))
-getDistributionInfo(rowSums(expression_data),
-                    "plots/data_distribution/", "stereoscope_synthetic_data", "continuous")
-getDistributionInfo(rowSums(expression_data),
-                    "plots/data_distribution/", "stereoscope_synthetic_data", "discrete")
+
 ## CELL2LOCATION ##
 # need a .h5ad file and an annotation (.csv) file
 # Creating h5ad file
@@ -177,28 +81,21 @@ metadata <- data.frame("cell" = colnames(brain_sc_10x),
 write.table(metadata, file="rds/seurat_obj_scrnaseq_cortex_filtered_rawcounts_metadata.csv",
             quote=FALSE, sep=",", col.names=TRUE, row.names=FALSE)
 
-# Run the pipeline (create_synthetic_data_cell2location.sh) using those two files #
-# Read in results
+# kidney
+kidney <- readRDS("Scripts/robin/raw_data/scRNAseq/seurat_obj_scrnaseq_kidney_filtered.rds")
+metadata <- data.frame("cell" = colnames(kidney),
+                       "bio_celltype" = kidney$celltype)
+write.table(metadata, file="rds/seurat_obj_scrnaseq_kidney_filtered_rawcounts_metadata.csv",
+            quote=FALSE, sep=",", col.names=TRUE, row.names=FALSE)
+
+
+# Run the pipeline (create_synthetic_data_cell2location.sh) using those two files
+# Read in results as follows
 library(stringr)
 # Get seeds
-file_names <- list.files("Data/synthetic_datasets/cell2location_sc_10x_braincortex_robin_rawcounts_2000spots/")
+file_names <- list.files("Data/synthetic_datasets/for_comparing_dists/cell2location_sc_10x_braincortex_robin_rawcounts/")
 seeds <- str_extract(gsub(".p", "", gsub(".*_", "", file_names)), regex("[0-9]+"))
 seeds <- unique(seeds[!is.na(seeds)])
-
-for (seed in seeds){
-  expression_data <- read.table(paste0("Data/synthetic_datasets/cell2location_sc_10x_braincortex_robin_rawcounts_2000spots/synthetic_ST_seed",
-                                       seed, "_1_counts.csv"), header=TRUE, sep=",", row.names=1)
-  png(file=paste0("plots/data_distribution/cell2location_seed", seed, "_2000spots_hist.png"))
-  print(hist(rowSums(expression_data), main="UMI distribution per spot of cell2location synthetic data"))
-  mtext(paste0("Mean: ", round(mean(rowSums(expression_data)),2),
-               "; SD: ", round(sd(rowSums(expression_data)), 2)), side=3)
-  dev.off()
-  print(paste0("Mean: ", round(mean(rowSums(expression_data)),2),
-               "; SD: ", round(sd(rowSums(expression_data)), 2)))
+expression_data <- read.table(paste0("Data/synthetic_datasets/for_comparing_dists/cell2location_sc_10x_braincortex_robin_rawcounts/2000spots/synthetic_ST_seed",
+                                       seeds[1], "_1_counts.csv"), header=TRUE, sep=",", row.names=1)
   
-  # had to remove lognormal distribution from the function
-  getDistributionInfo(rowSums(expression_data), "plots/data_distribution/",
-                      paste0("cell2location_synthetic_data_seed", seed), "continuous")
-  getDistributionInfo(rowSums(expression_data), "plots/data_distribution/",
-                      paste0("cell2location_synthetic_data_seed", seed), "discrete")
-}
