53c5e51bd0e113433c0afa4520d6d4430f108bff
csangara <sai.chananchida@gmail.com>
2021-05-13 23:21:01 +0700

    delete unnecessary files

--- Scripts/check_pseudobulk.R
+++
@@ -1,8 +0,0 @@
-library(Seurat)
-library(SeuratData)
-path <- "D:/Work (Yr 2 Sem 1)/Thesis/"
-cortex <- readRDS(paste0(path, "rds/cortex.rds"))
-allen_reference <- readRDS(paste0(path, "rds/allen_cortex.rds"))
-allen_reference <- SCTransform(allen_reference, ncells = 3000, verbose = FALSE)
-allen_reference <-  RunPCA(allen_reference, assay = "SCT", verbose = FALSE)
-allen_reference <-  RunUMAP(allen_reference, reduction = "pca", dims = 1:30)
