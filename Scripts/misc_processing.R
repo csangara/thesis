b8a3f9537061cdf137bc10c6be8dadc799d50f9a
csangara <sai.chananchida@gmail.com>
2021-05-13 23:34:24 +0700

    Update misc_processing.R
    cleaned up + added some code (UMAP of each dataset type, plot of sensitivity vs scale for each technology, etc..)

--- Scripts/misc_processing.R
+++ Scripts/misc_processing.R
@@ -1,13 +1,5 @@
 ### MISCELLANEOUS (PRE)PROCESSING SCRIPTS ###
-
-setwd("D:/Work (Yr 2 Sem 1)/Thesis/")
-path <- "D:/Work (Yr 2 Sem 1)/Thesis/"
-source("Scripts/helperFunctions.R")
-
-possible_dataset_types = c("real", "real_top1","real_top1_uniform","real_top2_overlap","real_top2_overlap_uniform",
-                           "real_missing_celltypes_visium", "artificial_uniform_distinct", "artificial_diverse_distinct",
-                           "artificial_uniform_overlap", "artificial_diverse_overlap", "artificial_dominant_celltype_diverse",
-                           "artificial_partially_dominant_celltype_diverse", "artificial_missing_celltypes_visium")
+source("D:/Work (Yr 2 Sem 1)/Thesis/Scripts/init.R")
 
 ### PREPROCESSING DATA ###
 # Preprocess scRNA reference data
@@ -28,7 +20,8 @@ DimPlot(seurat_obj_scRNA, reduction = "umap", label = TRUE, group.by = "celltype
 
 ### EXPLORING SYNTHETIC VISIUM DATA ###
 dataset_type <- "artificial_diverse_distinct"
-synthetic_visium_data <- readRDS("rds/synthvisium_spatial/allen_cortex_dwn_", dataset_type, "_synthvisium.rds")
+synthetic_visium_data <- readRDS(paste0("Data/synthetic_datasets/allen_cortex_dwn/allen_cortex_dwn_", dataset_type, "_synthvisium.rds"))
+synthetic_visium_data <- readRDS(paste0("Data/synthetic_datasets/kidney_generation/rep1/kidney_generation_", dataset_type, "_synthvisium.rds"))
 
 # Explore synthetic visium data
 synthetic_visium_data$counts %>% as.matrix() %>% .[1:5,1:5] #  Gene counts for each spot
@@ -45,27 +38,57 @@ p_priorregion = DimPlot(seurat_obj_visium, reduction = "umap", label = TRUE, gro
 p_exprs_clusters = DimPlot(seurat_obj_visium, reduction = "umap", label = TRUE) #
 patchwork::wrap_plots(list(p_priorregion, p_exprs_clusters), nrow = 1)
 
+### PLOTTING REGION COMPOSITIONS ON UMAP ###
+
+dataset_types = c("artificial_uniform_distinct", "artificial_diverse_distinct", 
+                  "artificial_uniform_overlap", "artificial_diverse_overlap",
+                  "artificial_dominant_celltype_diverse",
+                  "artificial_partially_dominant_celltype_diverse",
+                  "artificial_dominant_rare_celltype_diverse",
+                  "artificial_regional_rare_celltype_diverse")
+ds_list <- list()
+for (dataset_type in dataset_types){
+  synthetic_visium_data <- readRDS(paste0("Data/synthetic_datasets/cerebellum_cell_generation/rep3/cerebellum_cell_generation_", dataset_type, "_synthvisium.rds"))
+  spot_comp <- synthetic_visium_data$relative_spot_composition
+  
+  # Create seurat obj and UMAP
+  seurat_obj_visium <- createSeuratFromCounts(synthetic_visium_data$counts)
+  p <- DimPlot(seurat_obj_visium, reduction = "umap", label = FALSE, group.by = "orig.ident")
+  
+  #png(paste0("plots/UMAP_synthvisium_cer_cell/", dataset_type, "_no_comp.png"))
+  print(p + ggtitle(dataset_type) + theme(legend.position="none"))
+  #dev.off()
+  # Get all celltypes
+  # celltypes <- colnames(spot_comp)[1:(ncol(spot_comp)-2)]
+  # FeaturePlot(seurat_obj_visium, celltypes)
+  
+  region_comp = c()
+  for (region in paste0("priorregion", 1:5)){
+    temp_spot_comp <- spot_comp[spot_comp$region==region,1:(ncol(spot_comp)-2)]  
+    mean_comp <- apply(temp_spot_comp, 2, mean)
+    mean_comp <- mean_comp[mean_comp != 0]
+    sd_comp <- apply(temp_spot_comp, 2, sd)
+    sd_comp <- sd_comp[sd_comp != 0]
+    comp_text <- paste(names(mean_comp), round(mean_comp, 2), sep = ":", collapse = "; ")
+    
+    print(comp_text)
+    print(paste(names(sd_comp), round(sd_comp, 2), sep = ":", collapse = "; "))
+    
+    umap_coords <-seurat_obj_visium@reductions$umap@cell.embeddings[seurat_obj_visium$orig.ident==region,]
+    median_umap <- apply(umap_coords, 2, median)
+    p <- p + annotate("text", label=comp_text, x=median_umap[1], y=median_umap[2])
+    region_comp[region] <- comp_text
+  }
+  print(p + ggtitle(dataset_type))
+  ds_list[[dataset_type]] <- region_comp
+}
+
+ds_df <- t(data.frame(ds_list))
+write.table(ds_df, "plots/UMAP_synthvisium_cer_cell/comp.tsv", quote=FALSE, sep="\t")
+
 ### DOWNSAMPLING SPOTS FOR CIBERSORT ###
 input_path <- "D:/Work (Yr 2 Sem 1)/Thesis/Data/CIBERSORTx/cibersort_mixture_ArtUniDistinct.txt"
 reduceSpotsCS(input_path, 75)
-
-### CONVERTING SEURAT OBJECT TO LOOM ###
-for (dataset_type in possible_dataset_types){
-  print(dataset_type)
-  convertSeuratRDSToLoom(paste0(path, "rds/synthvisium_spatial/allen_cortex_dwn_", dataset_type,
-                                "_synthvisium.rds"), isSeurat = FALSE)
-}
-
-## SAVING NON-NORMALIZED DATA AS H5AD ##
-# Reference file
-convertSeuratRDSToh5ad("D:/Work (Yr 2 Sem 1)/Thesis/allen_cortex_dwn_original.rds")
-
-# Synthvisium files
-for (dataset_type in possible_dataset_types[10:13]){
-  print(dataset_type)
-  input_path <- paste0(path, "rds/synthvisium_spatial/allen_cortex_dwn_", dataset_type, "_synthvisium.rds")
-  convertSeuratRDSToh5ad(input_path, isSeurat = FALSE, PP= FALSE, raw=TRUE)
-}
 
 #### PLOT TIME OF CIBERSORT ####
 
@@ -75,19 +98,17 @@ ggplot(df, aes(x=x, y=y)) + geom_line() + geom_point() + xlab("Number of spots")
   labs(title="CIBERSORT runtime in function of spots")
 
 ######## PLOT PROPS #########
-# library(ggplot2)
-# 
-# n = nrow(decon_mtrx)
-# knownP = synthetic_visium_data$relative_spot_composition[1:n,1:23]
-# predP = decon_mtrx[1:n,1:23]
-# new_pred = data.frame("pred" = c(t(predP)))
-# new_pred$known = as.numeric(c(t(knownP)))
-# new_pred$celltype = rep(colnames(knownP), n)
-# 
-# ggplot(new_pred, aes(x=known, y=pred, shape=celltype)) +
-#   geom_abline(slope=1, intercept=0, linetype=2, colour="gray20") +
-#   scale_shape_manual(values = 1:23) + geom_point(size=1) +
-#   labs(x="Known Proportions", y="Predicted Proportions")
+n = nrow(decon_mtrx)
+knownP = synthetic_visium_data$relative_spot_composition[1:n,1:23]
+predP = decon_mtrx[1:n,1:23]
+new_pred = data.frame("pred" = c(t(predP)))
+new_pred$known = as.numeric(c(t(knownP)))
+new_pred$celltype = rep(colnames(knownP), n)
+
+ggplot(new_pred, aes(x=known, y=pred, shape=celltype)) +
+  geom_abline(slope=1, intercept=0, linetype=2, colour="gray20") +
+  scale_shape_manual(values = 1:23) + geom_point(size=1) +
+  labs(x="Known Proportions", y="Predicted Proportions")
 
 #### UMAP of generation data ####
 path <- "D:/Work (Yr 2 Sem 1)/Thesis/Data/synthetic_datasets/"
@@ -139,3 +160,20 @@ for (i in 1:length(datasets)){
 ggplot(df, aes(x=counts, color=dataset, fill=dataset)) + geom_density(alpha=0.2) +
   #geom_text(data = a, aes(x = xmax, y = ymax, label = dataset)) + xlim(0, 25000)
   scale_x_continuous(trans='log10') + labs(title=foi)
+
+
+### INTRODUCTION FIGURE - COMPARISON OF TECHNOLOGIES ###
+library(ggplot2)
+library(ggsci)
+library(stringr)
+data <- read.csv("Misc/comparingtechs.csv", quote="")
+data <- data[-9,]
+colnames(data)[1]="Technology"
+data$Approach <- str_replace(data$Approach, "Transcriptome", "Untargeted")
+ggplot(data, aes(x=Detection.efficiency, y=Genes, color=Approach, shape=Type)) +
+  geom_point(size=3) + scale_y_log10() +
+  #geom_text(aes(label=Technology, hjust=3, vjust=-0.3)) +
+  scale_x_continuous(breaks=c(0, 100), labels=c("Low", "High")) + xlab("Sensitivity") + ylab("Number of genes") +
+  #theme(panel.background = element_blank(), axis.line = element_line(size = 1, linetype = "solid"))
+  theme_bw() + scale_color_npg() +
+  guides(color = guide_legend(order=1), shape=guide_legend(order=2))
