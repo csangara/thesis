362fe986616cee2ad28b7451289714bfc8c7dcf2
csangara <sai.chananchida@gmail.com>
2021-05-17 11:21:59 +0700

    changed ds files for scref

--- Scripts/downsample_dataset.R
+++ Scripts/downsample_dataset.R
@@ -13,11 +13,11 @@ downsampleSynthvisium <- function(counts, n_cells, n_features, seed=10){
   return (counts[feature_names, cell_names])
   
 }
-
+seurat_obj_scRNA <- readRDS("Data/synthetic_datasets/generation_set/", dataset, ".rds")
 # Generate synthetic data with at least 10000 spots
 #synthetic_visium_data <- generate_synthetic_visium(seurat_obj_scRNA, dataset_type,
 #                                                   "celltype", n_regions=8, n_spots_min=2000, n_spots_max=2500)
-synthvisium_filepath <- paste0("downsampling/", str_replace(dataset, "_generation", ""),
+synthvisium_filepath <- paste0("downsampling/", str_replace(dataset, "_generation", ""), "/", repl,
                                "/brain_cortex_art_uni_distinct_18594spots_17538genes.rds")
 #saveRDS(synthetic_visium_data, synthvisium_filepath)
 synthetic_visium_data <- readRDS(synthvisium_filepath)
@@ -25,14 +25,18 @@ synthetic_visium_data <- readRDS(synthvisium_filepath)
 # Convert to h5ad
 convertSeuratRDSToh5ad(synthvisium_filepath, output_path=NULL, isSeurat=FALSE, raw=FALSE, update=FALSE)
 
-# Make dataframe of combinations (10 cells & 10 genes, 10 cells & 100 genes, etc...)
-all_scales <- gtools::permutations(4,2,10^(1:4), repeats.allowed=TRUE)
+# Make dataframe of combinations
+genes_comb <- c(1000, 5000, 10000, 15000)
+spots_comb <- c(100, 1000, 5000, 10000)
+perms <- gtools::permutations(4,2, repeats.allowed=TRUE)
+all_scales <- cbind(spots_comb[perms[,1]], genes_comb[perms[,2]])
+
 colnames(all_scales) <- c("n_cells", "n_genes")
 
 # Write conversion file (only run this once)
-# write.table(all_scales, file = paste0("downsampling/", str_replace(dataset, "_generation", ""),
-#                                       "/synthvisium_downsample_info/conversion.txt"),
-#                                       sep="\t", quote = FALSE, row.names = TRUE, col.names = TRUE)
+write.table(all_scales, file = paste0("downsampling/", str_replace(dataset, "_generation", ""),
+                                       "/synthvisium_conversion.txt"),
+                                       sep="\t", quote = FALSE, row.names = TRUE, col.names = TRUE)
 
 # Generate list of cells and genes for each case
 for (k in 1:nrow(all_scales)){
@@ -42,10 +46,10 @@ for (k in 1:nrow(all_scales)){
   ds_count <- downsampleSynthvisium(synthetic_visium_data$counts, n_cells, n_features)
 
   write.table(colnames(ds_count), file = paste0("downsampling/", str_replace(dataset, "_generation", ""),
-              "/synthvisium_downsample_info/cells", k, ".txt"),
+              "/", repl, "/synthvisium_downsample_info/cells", k, ".txt"),
               quote = FALSE, row.names = FALSE, col.names = FALSE)
   write.table(rownames(ds_count), file = paste0("downsampling/", str_replace(dataset, "_generation", ""),
-              "/synthvisium_downsample_info/genes", k, ".txt"),
+              "/", repl, "/synthvisium_downsample_info/genes", k, ".txt"),
               quote = FALSE, row.names = FALSE, col.names = FALSE)
 }
 
@@ -63,15 +67,16 @@ downsampleSCref <- function(cells_df, genes, n_cells, n_features, seed=10){
   
 }
 
-all_scales <- gtools::permutations(4,2,sort(c(10^(2:4), 5000)), repeats.allowed=TRUE)
+all_scales <- gtools::permutations(3,2,c(1000, 5000, 10000), repeats.allowed=TRUE)
 colnames(all_scales) <- c("n_cells", "n_genes")
 # Write conversion file (only run this once)
 # write.table(all_scales, file = paste0("downsampling/", str_replace(dataset, "_generation", ""),
-#                                       "/scref_downsample_info/conversion.txt"),
+#                                       "/scref_conversion.txt"),
 #                                        sep="\t", quote = FALSE, row.names = TRUE, col.names = TRUE)
 
 # Only get the names of cells and genes (and metadata) to save space
-seurat_obj_scRNA <- readRDS("rds/seurat_obj_scrnaseq_cortex_filtered.rds")
+# seurat_obj_scRNA <- readRDS("rds/seurat_obj_scrnaseq_cortex_filtered.rds")
+seurat_obj_scRNA <- readRDS("Scripts\\robin\\raw_data\\brain_cortex\\scRNAseq\\seurat_obj_scrnaseq_cortex_filtered.rds")
 cells_df <- data.frame(cells=colnames(seurat_obj_scRNA), celltype = seurat_obj_scRNA$subclass)
 genes <- rownames(seurat_obj_scRNA)
 
@@ -82,10 +87,10 @@ for (k in 1:nrow(all_scales)){
   print(c(n_cells, n_features))
   ds_object <- downsampleSCref(cells_df, genes, n_cells, n_features)
   
-  write.table(ds_object$cells, file = paste0("downsampling/", str_replace(dataset, "_generation", ""),
+  write.table(ds_object$cells, file = paste0("downsampling/", str_replace(dataset, "_generation", ""), "/", repl,
                                                 "/scref_downsample_info/ref_cells", k, ".txt"),
               quote = FALSE, row.names = FALSE, col.names = FALSE)
-  write.table(ds_object$genes, file = paste0("downsampling/", str_replace(dataset, "_generation", ""),
+  write.table(ds_object$genes, file = paste0("downsampling/", str_replace(dataset, "_generation", ""), "/", repl,
                                                 "/scref_downsample_info/ref_genes", k, ".txt"),
               quote = FALSE, row.names = FALSE, col.names = FALSE)
 }
@@ -97,3 +102,6 @@ df <- data.frame(vals = c(table(ds_object$cells), table(seurat_obj_scRNA$celltyp
                  source=rep(c("downsampled", "full"), each=length(unique(seurat_obj_scRNA$celltype))))
 ggplot(df, aes(fill=celltypes, y=vals, x=source)) + 
   geom_bar(position="fill", stat="identity")
+
+meta <- read.csv("rds/seurat_obj_scrnaseq_cortex_filtered_rawcounts_metadata.csv", header=TRUE)
+length(unique(meta$bio_celltype))
