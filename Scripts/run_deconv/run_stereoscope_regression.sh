3b1b908780b982605b096af97cf4f661b6c7fe31
Chananchida.Sangaram <chananchidas@cp0293.irc.ugent.be>
2021-05-18 03:16:09 +0200

    minor change to stereoscope regression file

--- Scripts/run_deconv/run_stereoscope_regression.sh
+++ Scripts/run_deconv/run_stereoscope_regression.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "$#" -lt 4 ]; then
   echo "This script creates a model using the single-cell data"
-  echo "USAGE: run_stereoscope_regression.sh < path to sc file > < covariate column name > < output directory > < cuda device >"
+  echo "USAGE: run_stereoscope_regression.sh < path to sc file > < covariate column name > < output directory > < cuda device > < save_time >"
   exit
 fi
 
@@ -16,4 +16,9 @@ OUT_DIR=$3
 echo "Reading $SC_PATH"
 mkdir -p $OUT_DIR
 
+SECONDS=0
 stereoscope run --sc_cnt $SC_PATH --label_colname $COV_NAME -o $OUT_DIR -n 5000 --gpu
+
+if [ "${@: -1}" = "true" ]; then
+  echo $SECONDS > $OUT_DIR/../model_stereoscope_info.txt
+fi
