141158ab367d99f1eef2c9092c5f47a35865c074
csangara <sai.chananchida@gmail.com>
2021-05-20 18:56:56 +0700

    Create run_deconv_prism_RCTD_liver.R
    for running rctd on liver

---
+++ Scripts/run_deconv/run_deconv_prism_RCTD_liver.R
@@ -0,0 +1,42 @@
+#!/usr/bin/env Rscript
+
+setwd("~/thesis/")
+path <- "~/data/"
+library(Seurat)
+library(dplyr)
+library(stringr)
+source("Scripts/helperFunctions.R")
+
+# Load in file using Seurat, and convert to SpatialRNA (do this locally)
+# See: https://rdrr.io/github/dmcable/RCTD/src/R/SpatialRNA.R (read.VisiumSpatialRNA)
+# seurat_obj_visium <- Load10X_Spatial("Data/Liver/JBO01")
+# coords <- seurat_obj_visium@images$slice1@coordinates
+# coords <- coords[,c("row", "col")]
+# colnames(coords) <- c("x", "y")
+# 
+# GetAssayData(seurat_obj_visium)
+# spatialRNA_obj_visium <- RCTD:::SpatialRNA(coords = coords,
+#                                        counts = GetAssayData(seurat_obj_visium))
+# saveRDS(spatialRNA_obj_visium, "Data/Liver/JBO01_spatialRNA.rds")
+
+results_path <- paste0("~/data/Liver/results/")
+scrna_path <- "~/data/Liver/anndatafineAnnot.rds"
+
+######### RCTD ##########
+
+library(RCTD)
+library(Matrix)
+dir.create(paste0(results_path, "RCTD/"), recursive=TRUE)
+
+seurat_obj_scRNA <- readRDS(scrna_path)
+DefaultAssay(seurat_obj_scRNA) <- "RNA"
+seurat_obj_scRNA@meta.data$liger_ident_coarse <- factor(seurat_obj_scRNA@meta.data$fine_annot)
+seurat_obj_scRNA@meta.data$nUMI <- colSums(seurat_obj_scRNA@assays$RNA@counts)
+
+spatialRNA_obj_visium <- readRDS("~/data/Liver/JBO01_spatialRNA.rds")
+
+RCTD_deconv <- create.RCTD(spatialRNA_obj_visium, seurat_obj_scRNA, max_cores = 8)
+RCTD_deconv <- run.RCTD(RCTD_deconv, doublet_mode = FALSE)
+res = as.matrix(sweep(RCTD_deconv@results$weights, 1, rowSums(RCTD_deconv@results$weights), '/'))
+
+saveRDS(res, paste0(results_path, "RCTD/liver_deconv_RCTD.rds"))
