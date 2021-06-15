74ee1c96d39113ab1ea2df12d062fc11ab50234a
csangara <sai.chananchida@gmail.com>
2021-06-15 22:33:00 +0700

    Update evaluation_per_rep.R

--- Scripts/evaluation_per_rep.R
+++ Scripts/evaluation_per_rep.R
@@ -1,7 +1,9 @@
+#### DETAILED PLOTS OF EACH REPLICATE ####
+source("D:/Work (Yr 2 Sem 1)/Thesis/Scripts/init.R")
 
 ###### PLOT PREDICTIONS ON UMAP #######
 for (dataset in datasets[2:length(datasets)]){
-  for (repl in paste0('rep', 1:10)){
+  for (repl in paste0('rep', 4:10)){
     result_path <- paste0("results/", dataset, "/", repl, "_", run, "/")
     for (dataset_type in possible_dataset_types){
       
@@ -53,10 +55,9 @@ for (dataset in datasets[2:length(datasets)]){
   }
 }
 
-
 #### PLOT DISTRIBUTION OF CORRELATION ####
 for (dataset in datasets[2:length(datasets)]){
-  for (repl in paste0('rep', 1:10)){
+  for (repl in paste0('rep', 4:10)){
     result_path <- paste0("results/", dataset, "/", repl, "_", run, "/")
     for (dataset_type in possible_dataset_types){
       # Load reference data and deconvolution results
@@ -87,82 +88,29 @@ for (dataset in datasets[2:length(datasets)]){
   }
 }
 
-#### PRINT OUT RESULTS IN A TABLE ####
-library(xlsx)
-for (repl in paste0("rep", 1:10)){
-  wb = createWorkbook()
-  for (ds in datasets[-1]){
-    res_path <- paste0("results/", ds, "/", repl, "_", run, "/")
-    
-    all_results <- readRDS(paste0(res_path, "all_metrics_", ds, ".rds"))
-    sheet = createSheet(wb, ds)
-    i = 1
-    for (dataset_type in names(all_results)){
-      addDataFrame(data.frame(x=dataset_type), sheet=sheet, startRow=i,
-                   row.names=FALSE, col.names=FALSE)
-      temp_data = t(all_results[[dataset_type]])
-      addDataFrame(temp_data, sheet=sheet, startRow=i+1)
-      i = i + nrow(temp_data) + 2
-    }
-  }
-  dir.create(paste0("results/summary files/", repl, "_", run, "/"), recursive=TRUE, showWarnings = FALSE)
-  saveWorkbook(wb, paste0("results/summary files/", repl, "_", run, "/all_results.xlsx"))
-}
 
-#### PLOT RESULTS BY DATASET ####
-library(stringr)
+#### PLOT EACH METRIC AS FACET ####
+metrics <- c("RMSE", "accuracy", "sensitivity", "specificity", "precision", "F1", "prc")
 
-for (repl in paste0("rep", 1:10)){
-  df <- data.frame()
-  for (ds in datasets[-1]){
-    res_path <- paste0("results/", ds, "/", repl, "_", run, "/")
-    all_results <- readRDS(paste0(res_path, "all_metrics_", ds, ".rds"))
-    metric <- "RMSE"
-    methods <- rownames(all_results[[1]])
-    temp_df <- data.frame(sapply(all_results, function (k) k[metric]))
-    temp_df <- data.frame(y=apply(temp_df, 1, mean),
-                          ymin=apply(temp_df, 1, min),
-                          ymax=apply(temp_df, 1, max),
-                          method=methods,
-                          dataset=rep(str_replace(ds, "_generation", ""), 
-                                      length(methods)))
-    df <- rbind(df, temp_df)
-  }
-  df$dataset <- str_replace(df$dataset, "cerebellum_", "cer_")
-  df$dataset <- str_replace(df$dataset, "hippocampus", "hipp")
-  dir.create(paste0("results/summary files/", repl, "_", run, "/"), recursive=TRUE, showWarnings = FALSE)
-  png(paste0("results/summary files/", repl, "_", run, "/all_datasets_", metric, ".png"), width=800, height=500)
-  print(ggplot(data=df, aes(x=dataset, y=y, color=method)) +
-          geom_point(size=2, position=position_dodge(.5)) +
-          geom_errorbar(aes(ymin=ymin, ymax=ymax), width=.2, position=position_dodge(.5)) +
-          labs(title=paste0(metric, " of different methods on all datasets")) + ylab(metric) + xlab("datasets"))
-  dev.off()
-}
+for (dataset in datasets[2:length(datasets)]){
+  for (repl in paste0("rep", 1:10)){
+    result_path <- paste0("results/", dataset, "/", repl, "_", run, "/")
+    
+    if (!dir.exists(paste0(result_path, "plots/"))){ dir.create(paste0(result_path, "plots/"), recursive=TRUE) }
+    
+    all_results <- readRDS(paste0(result_path, "all_metrics_", dataset, ".rds"))
+    all_results2 <- melt(all_results) %>% mutate("dataset_type"=str_replace(L1, "artificial_", "") %>%
+                                                   factor(., levels=unique(.))) %>%
+                                          filter(variable %in% metrics)
+    all_results2$methods <- methods
+    
+    p <- ggplot(all_results2, aes(x=methods, y=value, shape=dataset_type, color=methods)) +
+      scale_shape_manual(values=1:length(possible_dataset_types)) + geom_jitter(width=0.1) +
+      facet_wrap(vars(variable), scales="free_y")
+    
+    png(paste0(result_path, "plots/all_metrics.png"), width=800, height=450)
+    print(p)
+    dev.off()
 
-#### PLOT RESULTS BY DATASET TYPE ####
-for (repl in paste0("rep", 1:10)){
-  df <- data.frame()
-  metric <- "RMSE"
-  for (dataset_type in possible_dataset_types){
-    all_results <- lapply(datasets[-1], function(k) {
-      readRDS(paste0("results/", k, "/", repl, "_", run, "/all_metrics_", k, ".rds"))})
-    temp_df <- data.frame(sapply(all_results, function(k) k[[dataset_type]][[metric]]))
-    colnames(temp_df) <- datasets[-1]
-    
-    methods <- rownames(all_results[[1]][[1]])
-    
-    temp_df <- data.frame(y=apply(temp_df, 1, mean),
-                          ymin=apply(temp_df, 1, min),
-                          ymax=apply(temp_df, 1, max),
-                          method=methods)
-    df <- rbind(df, temp_df)
   }
-  df$dataset_type = rep(1:length(possible_dataset_types), each=length(methods))
-  
-  png(paste0("results/summary files/", repl, "_", run, "/all_dataset_types_", metric, ".png"), width=800, height=500)
-  print(ggplot(data=df, aes(x=factor(dataset_type), y=y, color=method)) +
-          geom_point(size=2, position=position_dodge(.5)) +
-          geom_errorbar(aes(ymin=ymin, ymax=ymax), width=.2, position=position_dodge(.5)) +
-          labs(title=paste0(metric, " of different methods on all dataset types")) + ylab(metric) + xlab("dataset type"))
-  dev.off()
 }
